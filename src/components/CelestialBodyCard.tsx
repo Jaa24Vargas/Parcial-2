9f9d238d7a2d1b76b76f5c07fa6102068a51537f
JaAn24 <vargasjaime0224@hotmail.com>
2024-10-10 18:20:22 -0500

    Lo intente, trabajo Honesto

--- src/components/CelestialBodyCard.tsx
+++ src/components/CelestialBodyCard.tsx
@@ -10,8 +10,8 @@ const CelestialBodyCard: React.FC<CelestialBodyCardProps> = ({ body }) => {
   return (
     <Link to={`/cuerpo/${body.id}`} className="card">
       <h3>{body.name}</h3>
-      <p>Gravedad: {body.gravity} m/s²</p>
-      <p>Diámetro: {body.diameter} km</p>
+      <p>Gravity: {body.gravity} m/s²</p>
+      <p>Diameter: {body.diameter} km</p>
     </Link>
   );
 };
